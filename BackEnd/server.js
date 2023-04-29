d73d137bb26f1daeccbd648432e26109c48fda8e
henockso111@gmail.com <henockso111@gmail.com>
2023-04-29 16:13:59 +0100

    backend start

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,14 +1,33 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const port= process.env.PRIVATE_URL;
+const dotenv = require('dotenv');
+const port = process.env.PORT || 8000;
 
 
 
 const app = express();
 app.use(express.json());
+dotenv.config();
 
 
 
+(async () => {
+  try {
+    await mongoose.connect(process.env.SERVER_URI);
+    console.log('Connected to database');
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
+app.get ('/', (req, res) => {
+    try {
+        const message = 'connection is working';
+        res.send(message);
+    } catch(error){
+        res.send(error);
+    }
+});
 
 
 
@@ -24,5 +43,5 @@ app.use(express.json());
 
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
+  console.log(`Server is running on port ${port}`);
+});
